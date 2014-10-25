d70afe4f5593ee4b02874442a59c7d383ee84341
Chris Buenger <buenger@cruise-systems.com>
2014-10-26 00:33:12 +0200

    Initial Commit

---
+++ libais/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  libais
+//
+//  Created by Chris Bünger on 26/10/14.
+//  Copyright (c) 2014 Chris Bünger. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
