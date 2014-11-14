c54ba10555c6d7ddd39d0c7a3e3b6953a87cde3d
Chris Buenger <buenger@cruise-systems.com>
2014-11-14 22:12:14 +0100

    Allow decoding of multiple (MAXDEVICES) sources in parallel.

--- libais/libais-python.c
+++ libais/libais-python.c
@@ -12,15 +12,38 @@
 #include "libais.h"
 //#include "gps.h"
 
-static struct gps_device_t session;
+#define MAXDEVICES	1024
+
+static struct gps_device_t session[MAXDEVICES];
+
+static bool assigned[MAXDEVICES];
 
 static PyObject*
-libais_decode(PyObject* self, PyObject* args)
+libais_decode(PyObject* self, PyObject* args, PyObject *kwargs)
 {
     const char* msg;
+    int decoderId=-1;
     
-    if (!PyArg_ParseTuple(args, "s", &msg))
+    static char *kwlist[] = {"message","decoderId", NULL};
+    
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s|i", kwlist, &msg, &decoderId))
         return NULL;
+
+    if ( decoderId == -1 ) {
+        printf("Warning: No decoder selected. Using default decoder '0'.\n");
+        decoderId=0;
+        if (!assigned[0]) {
+            assigned[0]=true;
+        } else {
+            printf("Error: Default decoder already assigned. Get your own!.\n");
+            return NULL;
+        }
+    }
+    
+    if (!assigned[decoderId]) {
+        printf("Warning: Selected decoder '%d' was not assigned. Now it is.\n", decoderId);
+        assigned[decoderId]=true;
+    }
     
 //    printf("Message: %s, length: %ul\n", msg, sizeof(msg));
     
@@ -31,7 +54,7 @@ libais_decode(PyObject* self, PyObject* args)
     
 //    printf("Buffer: %s, length: %ul\n", buf, sizeof(buf));
     
-    if (aivdm_decode(msg, strlen(msg)+1, &session, &ais, 1)) {
+    if (aivdm_decode(msg, strlen(msg)+1, &(session[decoderId]), &ais, 1)) {
 //      printf("type: %d, repeat: %d, mmsi: %d\n", ais.type, ais.repeat, ais.mmsi);
         json_aivdm_dump(&ais, NULL, true, buf, buflen);
 //      printf("JSON: %s", buf);
@@ -41,14 +64,50 @@ libais_decode(PyObject* self, PyObject* args)
     return Py_None;
 }
 
+static PyObject*
+libais_getDecoderId(PyObject* self)
+{
+    for (int i=0; i<MAXDEVICES; i++) {
+        if (assigned[i]==false) {
+            assigned[i]=true;
+            return Py_BuildValue("i", i);
+        }
+    }
+    // No unassigned decoders available
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject*
+libais_releaseDecoderId(PyObject* self, PyObject* args)
+{
+    int decoderId;
+    
+    if (!PyArg_ParseTuple(args, "i", &decoderId))
+        return NULL;
+    
+    if (assigned[decoderId]==true) {
+        assigned[decoderId]=false;
+        return Py_BuildValue("i", 0);
+    }
+    return NULL;
+}
+
 static PyMethodDef libais_methods[] =
 {
-    {"decode", libais_decode, METH_VARARGS, "Decode AIVDM sentence."},
+    {"decode", (PyCFunction)libais_decode, METH_VARARGS|METH_KEYWORDS, "Decode AIVDM sentence."},
+    {"getDecoderId" , (PyCFunction)libais_getDecoderId, METH_NOARGS, "Get a decoder id. Returns 'None' if no decoders are available."},
+    {"releaseDecoderId", (PyCFunction)libais_releaseDecoderId, METH_VARARGS, "Give decoderId back."},
     {NULL, NULL, 0, NULL}
 };
 
 PyMODINIT_FUNC
 initlibais(void)
 {
+    // Initialize deocder assignment array
+    for (int i=0; i<MAXDEVICES; i++) {
+        assigned[i]=false;
+    }
+
     (void) Py_InitModule("libais", libais_methods);
 }
